c08e75b21407bde1424988a3bac890e522881205
Daffa <84918684+daffacode@users.noreply.github.com>
2021-12-26 15:56:08 +0700

    dev

--- dist/utils/Catalysts.d.ts
+++ dist/utils/Catalysts.d.ts
@@ -1,6 +1,7 @@
 import type { EthAddress } from 'mitz-auth';
 export declare function getMainnetCatalysts(): Promise<ServerMetadata[]>;
 export declare function getRopstenCatalysts(): Promise<ServerMetadata[]>;
+export declare function getHarmonyTestnetCatalysts(): Promise<ServerMetadata[]>;
 export declare type ServerMetadata = {
     address: string;
     owner: EthAddress;
